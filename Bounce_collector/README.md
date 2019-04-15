b941af511d38e919bc072e60dfc12385b22e8f0a
weizhang2017 <wzhang@leadbook.com>
2019-04-15 19:03:40 +0800

    add bounce collector

---
+++ Bounce_collector/README.md
@@ -0,0 +1,4 @@
+prerequisite: `pip install -i https://test.pypi.org/simple/ SMTPEmail`
+
+1. Collect Bounces from SMTP server 
+2. Classify email messages 
---
+++ Bounce_collector/bounce_collector.py
@@ -0,0 +1,73 @@
+from SMTPEmail import IMAP
+from Bounce_classifier.bounce_classifier import Bounce_classifier
+from mongoengine import *
+import datetime
+import requests
+
+class EmailServer:
+
+	def __init__(self,ip_addr,account,password):
+		self.IMAP_client = IMAP(
+						IMAP_server=ip_addr,
+						IMAP_account=account,
+						IMAP_password=password
+			)
+
+	def retrieve_bounce_smtp(self,mail_box):
+		#this task is for cron job to retrieve bounces from email server
+		try:
+
+			for msg in self.IMAP_client.retrieve_msg(delete=False,mailbox_name=mail_box, msg_id='all'):
+				if isinstance(msg[0], bytes):
+					message_format = msg[0].decode("utf-8", "ignore")
+				else:
+					message_format = str(msg[0])
+				if isinstance(msg[1], bytes):
+					message = msg[1].decode("utf-8", "ignore")
+				else:
+					message = str(msg[1])
+				yield message
+		except ValueError as e:
+			print(str(e))
+
+	@staticmethod
+	def retrieve_bounce_sendgrid(api_key):
+		url = 'https://api.sendgrid.com/v3/suppression/bounces'
+		res = requests.get(url, headers={'Authorization': 'Bearer {}'.format(api_key), 'accept': "application/json"})
+		print(res.content)
+
+class Message:
+	def __init__(self, message):
+		self.message = message
+
+	def classify(self):
+		bc = Bounce_classifier(self.message)
+		catalogue, key_word = bc.no_diagonostic_code()
+		return catalogue, key_word
+'''
+class Bounce(Document):
+
+	message = StringField(required=True)
+	catalogue = StringField(required=True)
+	key_word = StringField()
+	date_modified = DateTimeField(default=datetime.datetime.now(tz=datetime.timezone(datetime.timedelta(hours=8))))
+	meta = {'collection': 'bounce_messages'}
+
+class OperationBounceDB:
+	def __init__(self):
+		con = connect('bounce')
+
+	def store_message(self, message):
+		bc = Bounce_classifier(message)
+		catalogue, key_word = bc.no_diagonostic_code()
+		Bounce(message=message, catalogue=catalogue, key_word=key_word).save()
+
+class Main:
+	@staticmethod
+	def collect_bounce():
+		es = EmailServer('128.199.82.10','marketing@largesell.xyz','123welcome123')
+		for message in es.retrieve_bounce_smtp('inbox'):
+			odb = OperationBounceDB()
+			odb.store_message(message)
+			print(message)
+'''
